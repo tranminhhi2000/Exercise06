cd5246192b03f45ef8d2c4f579c06a5c65ab795e
tranminhhi2000 <tranminhhi2732000@gmail.com>
2019-10-29 13:37:18 +0700

    29/10/2019

---
+++ Exercise06-B/Problem3/Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Queue
+    {
+        public class Queue
+        {
+            private int[] buffer;
+            private int top;
+            private int p;
+
+            public Queue()
+            {
+                this.buffer = new int[10];
+                this.top = 0;
+            }
+
+            public Queue(int p)
+            {
+                // TODO: Complete member initialization
+                this.p = p;
+            }
+            public void Enqueue(int n)
+            {
+                buffer[top - 1] = n;
+            }
+            public int Dequeue()
+            {
+                int n = buffer[0];
+                for (int i = 0; i < top; i++)
+                    buffer[i] = buffer[i + 1];
+                top--;
+                return n;
+            }
+            public int peek()
+            {
+                return buffer[0];
+            }
+            public int count()
+            {
+
+                return top + 1;
+            }
+            public void clear()
+            {
+                top = -1;
+            }
+
+        }
+
+        internal void Enqueue(int p)
+        {
+            throw new NotImplementedException();
+        }
+        internal int Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exercise06-B/UnitTestProblem3/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace UnitTestProblem3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPush()
+        {
+            Queue s = new Queue(10);
+
+
+        }
+        [TestMethod]
+        public void TestPop()
+        {
+            Queue s = new Queue(10);
+
+        }
+        [TestMethod]
+        public void TestPeek()
+        {
+            Queue s = new Queue(10);
+
+        }
+        [TestMethod]
+        public void TestClear()
+        {
+            Queue s = new Queue(10);
+
+        }
+    }
+}
